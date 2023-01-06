49577777613926be1c5dd2b29a6a2c802e12bc4b
zaraysf <e402_404@yahoo.com>
2023-01-06 22:11:26 +1100

    writing javascript code for validation firstName

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-function validateForm() {
-    let x = document.forms["myForm"]["firtName"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+function validateFirstName(){
+
+var firstName=document.getElementById('firstName');
+var stringf = document.getElementById('firstName').value;
+if (firstName.value=="") {
+     alert("firstName name must be filled out");
+     return false;
+    }
+else if (document.getElementById('firstName').value.length > 20){
+        alert("firstName name cannot be more than 35 characters");
+        return false;
+    }
+    else if (/^[a-z][a-z '-.,]{0,20}$|^$/i.test( stringf )){
+
     }
   }
+
+const btnSubmit = document.getElementById(submit);
+btnSubmit.addEventListener('click' , (e)=>{
+  alert('Sorry! you cannot check this checkbox because of the preventDefault.');
+  e.preventDefault();
+validateFirstName();
+console.log(validateFirstName());
+})
+
+
+
